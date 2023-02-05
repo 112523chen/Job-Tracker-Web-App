7bd220f8e3d68ed262f2241bf58b8899bcbd3d4b
Alex Chen <112623chen@gmail.com>
2023-02-05 01:27:14 -0500

    ✨ new(): add fetching applications call

---
+++ client/src/helper/functions.ts
@@ -0,0 +1,7 @@
+import { application } from "../components/model";
+
+export const getApplicationData = async (): Promise<application[]> => {
+  const response = await fetch("http://localhost:3000/applications");
+  let applications = (await response.json()) as application[];
+  return applications;
+};
