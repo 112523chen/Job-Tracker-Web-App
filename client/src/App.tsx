6f06a7d705cfa85d1719cb29c654e127891e9f89
Alex Chen <112623chen@gmail.com>
2023-02-24 09:33:24 -0500

    🚀 new(): add routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,9 +2,15 @@ import MainView from "./components/MainView/MainView";
 import { application } from "./components/model";
 import { useState, useEffect } from "react";
 import { getApplicationData } from "./helper/functions";
+import AddView from "./components/AddView/AddView";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RecentView from "./components/RecentView/RecentView";
+import AnalyticsView from "./components/AnalyticsView/AnalyticsView";
 
 const App = () => {
   const [applications, setApplications] = useState<application[]>([]);
+  const [isInAddView, setIsInAddView] = useState<boolean>(false);
+  const [isInAppView, setIsInAppView] = useState<boolean>(false);
 
   const fetchApplications = async () => {
     const applicationData = await getApplicationData();
@@ -13,12 +19,52 @@ const App = () => {
 
   useEffect(() => {
     fetchApplications();
-    console.log(applications);
-  }, []);
+    setIsInAppView(false);
+  }, [isInAddView, isInAppView]);
 
   return (
     <div className="App">
-      <MainView applicationData={applications} />
+      {isInAddView ? (
+        <AddView setIsInAddView={setIsInAddView} isInAddView={isInAddView} />
+      ) : null}
+
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <MainView
+                applicationData={applications}
+                setIsInAddView={setIsInAddView}
+                isInAddView={isInAddView}
+                setIsInAppView={setIsInAppView}
+              />
+            }
+          />
+          <Route
+            path="/recent"
+            element={
+              <RecentView
+                applicationData={applications}
+                setIsInAddView={setIsInAddView}
+                isInAddView={isInAddView}
+                setIsInAppView={setIsInAppView}
+              />
+            }
+          />
+          <Route
+            path="/analytics"
+            element={
+              <AnalyticsView
+                applicationData={applications}
+                setIsInAddView={setIsInAddView}
+                isInAddView={isInAddView}
+                setIsInAppView={setIsInAppView}
+              />
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
