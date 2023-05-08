45c309ba54521abfd887b5b6cde502e48866d895
Alex Chen <112623chen@gmail.com>
2023-05-08 03:36:02 -0400

    test(): add test

--- client/src/helper/functions.test.ts
+++ client/src/helper/functions.test.ts
@@ -1,9 +1,32 @@
-import { isNotAvailable } from "./functions";
+import {
+  isNotAvailable,
+  getLabel,
+  getFormValue,
+  getApplicationColor,
+} from "./functions";
 
-describe("isNotAvailable Test", () => {
+describe("isNotAvailable Tests", () => {
   test("should return add view", () => {
     const isInAddView = true;
     const isInAppView = true;
     expect(isNotAvailable(isInAddView, isInAppView)).toEqual(true);
+  });
+});
+
+describe("getLabel Tests", () => {
+  test("should return Job Title", () => {
+    expect(getLabel("title")).toBe("Job Title");
+  });
+
+  test("should return Company", () => {
+    expect(getLabel("company")).toBe("Company");
+  });
+
+  test("should return Link", () => {
+    expect(getLabel("url")).toBe("Link");
+  });
+
+  test("should return Empty", () => {
+    expect(getLabel("Empty")).toBe("");
   });
 });
