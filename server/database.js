3e2da3187c3d7b7868c4a76602349b750e5d16d0
Alex Chen <112623chen@gmail.com>
2023-05-09 00:23:26 -0400

    test

---
+++ server/database.js
@@ -0,0 +1,24 @@
+const { Client } = require("pg");
+
+const pgclient = new Client({
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  user: "postgres",
+  password: "postgres",
+  database: "jobapp",
+});
+
+pgclient.connect();
+
+const table =
+  "CREATE TABLE applications ( id SERIAL primary key, title varchar, company varchar, created timestamptz, modified timestamptz, status varchar,url varchar, description varchar);";
+const data =
+  "INSERT INTO applications (title, company, created, modified, status, url, description) VALUES ('Software Engineer Intern', 'CUNY', NOW(), NOW(), 'created', 'https://112523chen.github.io', 'Some text'); INSERT INTO applications (title, company, created, modified, status, url, description) VALUES ('Software Engineer Intern', 'CUNY', NOW(), NOW(), 'applied', 'https://112523chen.github.io', 'Some text');";
+
+pgclient.query(table, (error, response) => {
+  if (error) throw error;
+});
+
+pgclient.query(data, (error, response) => {
+  if (error) throw error;
+});
