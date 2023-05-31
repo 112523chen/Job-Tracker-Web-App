365f95859f7e15f57749818bc8e1e85a3ff65f0a
Alex Chen <112623chen@gmail.com>
2023-05-31 11:12:56 -0400

    feature(): add data visualizations

--- client/src/pages/AnalyticsPage/AnalyticsSection.style.tsx
+++ client/src/pages/AnalyticsPage/AnalyticsSection.style.tsx
@@ -9,4 +9,5 @@ export const AnalyticsSectionBase = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
 `;
--- client/src/pages/AnalyticsPage/AnalyticsSection.tsx
+++ client/src/pages/AnalyticsPage/AnalyticsSection.tsx
@@ -1,30 +1,72 @@
 import React from "react";
-import { getBarChartData } from "../../helper/api/functions";
+import {
+  getBarChartData,
+  getBarChartDataByTimeFrame,
+  getBarChartTitleData,
+} from "../../helper/api/functions";
 import { useState, useEffect } from "react";
 import { barChartDataType } from "../../components/model";
 import BarGraph from "../../components/Graphs/BarGraph/BarGraph";
 import { AnalyticsSectionBase } from "./AnalyticsSection.style";
+import { TimeFrame } from "../../helper/models";
 
 interface Props {}
 
 const AnalyticsSection: React.FC<Props> = () => {
   const [barChartData, setBarChartData] = useState<barChartDataType[]>([]);
+  const [titleBarChartData, setTitleBarChartData] = useState<
+    barChartDataType[]
+  >([]);
+  const [currentTimeFrame, setCurrentTimeFrame] = useState<TimeFrame>("week");
 
   const fetchBarChartData = async () => {
-    const data = await getBarChartData();
-    console.log(data);
-    setBarChartData(data);
+    const roleData = await getBarChartDataByTimeFrame(currentTimeFrame);
+    setBarChartData(roleData);
+  };
+
+  const fetchBarChartTitleData = async () => {
+    const titleData = await getBarChartTitleData(5);
+    setTitleBarChartData(titleData);
+  };
+
+  const updateCurrentTimeFrame = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    const newTimeFrame: TimeFrame = event.target.value as TimeFrame;
+    setCurrentTimeFrame(newTimeFrame);
   };
 
   useEffect(() => {
-    fetchBarChartData();
+    fetchBarChartTitleData();
   }, []);
 
-  console.log(barChartData);
+  useEffect(() => {
+    fetchBarChartData();
+  }, [currentTimeFrame]);
 
   return (
     <AnalyticsSectionBase>
-      <BarGraph barChartData={barChartData} />
+      <BarGraph
+        barChartData={barChartData}
+        xAxisValueName={"status"}
+        xAxisLabel={"Status"}
+        yAxisLabel={"# of Roles"}
+        title={"Distribution of Status"}
+      />
+      {/* <BarGraph
+        barChartData={titleBarChartData}
+        xAxisValueName={"title"}
+        xAxisLabel={"Titles"}
+        yAxisLabel={"Count"}
+        title={"Most Popular Roles Applied to"}
+      /> */}
+      <p>Select Time Frame</p>
+      <select onChange={updateCurrentTimeFrame}>
+        <option value={"week"}>Last Week</option>
+        <option value={"month"}>Last Month</option>
+        <option value={"month3"}>Last 3 Month</option>
+        <option value={"month6"}>Last 6 Month</option>
+      </select>
     </AnalyticsSectionBase>
   );
 };
